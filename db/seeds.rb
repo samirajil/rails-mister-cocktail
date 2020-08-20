83014fb222eb8bfd0b8965c40c1b778198e5a3ae
Samira Eilinger <samira_eilinger@hotmail.com>
2020-08-20 12:03:48 +0100

    seed run

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'json'
+require 'byebug'
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+
+ingredients = JSON.parse(open(url).read)
+
+ingredients["drinks"].each do |ingredient|
+  description = ingredient["strIngredient1"]
+  Ingredient.create(name: description)
+end
